13097a12400be935a83644e3f318d10db3409d5e
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-26 09:09:02 +0700

    Initial commit

---
+++ lib/List/Util/PHP.pm
@@ -0,0 +1,61 @@
+package List::Util::PHP;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = qw(
+                       in_array
+                       is_array
+               );
+
+sub in_array {
+}
+
+sub in_array {
+}
+
+1;
+# ABSTRACT: PHP-style routines related to lists
+
+=head1 SYNOPSIS
+
+ use List::Util::PHP qw(in_array is_array);
+
+ my @ary = (1,3,"foo",7,2,"bar",10,'11',"baz");
+
+ if (in_array(3, \@ary)) {
+     ... # will match
+ }
+
+ if (in_array(11, \@ary, 'strict')) {
+     ... # will not match
+ }
+
+ if (is_array(\@ary)) {
+     ...
+ }
+
+
+=head1 DESCRIPTION
+
+Experimental.
+
+
+=head1 FUNCTIONS
+
+Not exported by default but exportable.
+
+=head2 in_array
+
+=head2 is_array
+
+
+=head1 SEE ALSO
+
+L<List::Util>
